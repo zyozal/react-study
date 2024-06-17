fee040a4247641aaaf973ef6149a6652a0d7f205
zyozal <zyozal@naver.com>
2024-06-17 14:06:09 +0900

    0617

--- src/App.js
+++ src/App.js
@@ -1,49 +1,32 @@
-import React, { useState } from 'react';
-import CourseInput from './components/CourseGoals/CourseInput';
-import CourseList from './components/CourseGoals/CourseList';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UserList';
+import ErrorModal from './components/UI/Modal/ErrorModal';
 
-// 기본 더미 데이터
-const DUMMY_DATA = [
-  {
-    id: 'g1',
-    text: '리액트 컴포넌트 스타일링 마스터하기'
-  },
-  {
-    id: 'g2',
-    text: 'UI/UX 프로그래밍 쌉고수되기'
-  },
-];
 
 const App = () => {
 
-  const [goals, setGoals] = useState(DUMMY_DATA);
+  // 회원들이 저장될 배열
+  const [userList, setUserList] = useState([]);
 
-  // CourseInput에게 전달할 함수
-  const addGoalHandler = (goalObject) => {
-    setGoals([...goals, goalObject]);
-  };
+  const addUserHandler = user => {
 
-  // CouseItem에게 전달할 함수
-  const deleteGoalHandler = (id) => {
-    // console.log('id: ', id);
-
-    // console.log('index: ', index);
-    // goals.splice(goals.findIndex(g => g.id === id), 1);
-
-    setGoals(goals.filter(g => g.id !== id));
-
+    console.log(user);
+    setUserList(prev => [
+      ...prev, 
+      {
+        ...user, 
+        id: Math.random().toString()
+      }
+    ]);
   };
 
   return (
-    <div>
-      <section id="goal-form">
-        <CourseInput onAdd={addGoalHandler} />
-      </section>
-      <section id="goals">
-        <CourseList items={goals} onDelete={deleteGoalHandler} />
-      </section>
-    </div>
+    <>
+      <AddUsers onAddUser={addUserHandler} />
+      <UserList users={userList} />
+    </>
   );
 };
 
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Button.css';
+import styles from './Button.module.css';
 
 const Button = ({ type, onClick, children}) => {
   return (
-    <button type={type} className="button" onClick={onClick}>
+    <button type={type} className={styles.button} onClick={onClick}>
       {children}
     </button>
   );
