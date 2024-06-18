ba7f9d64660c564af7f1f2ce17653eac30fdec49
zyozal <zyozal@naver.com>
2024-06-18 14:17:36 +0900

    0618

---
+++ src/components/SideEffect/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Navigation.module.css';
+
+const Navigation = ({ onLogout }) => {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <a href="/">MyPage</a>
+        </li>
+
+        <li>
+          <a href="/">Admin</a>
+        </li>
+
+        <li>
+          <button onClick={onLogout}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
