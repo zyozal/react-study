3cd9cbe70239b98d9c438f348a3ac289ec305967
zyozal <zyozal@naver.com>
2024-07-04 11:45:08 +0900

    0704

---
+++ src/components/RouteExample/pages/EditPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EventForm from '../components/EventForm';
+import { useRouteLoaderData } from 'react-router-dom';
+
+const EditPage = () => {
+
+  const ev = useRouteLoaderData('event-detail');
+
+  return <EventForm method='patch' event={ev} />;
+};
+
+export default EditPage;
